30f2379758c7bc6c2a8b10c8b3a1763d2b3a0463
QuanHao Guo <49911294+GuoQuanhao@users.noreply.github.com>
2021-12-19 14:16:30 +0800

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,40 @@
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_10.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_20.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_30.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_40.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_50.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_60.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_70.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_80.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_90.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
+python evaluate.py /home/aistudio/Data/wider_face_split/wider_face_val_bbx_gt.txt --dataset-root /home/aistudio/Data --checkpoint weights/checkpoint_100.pdparams --split val
+cd widerface_evaluate/
+python evaluation.py
+cd ..
